644ad579c3a86b67eb24b365de8374fb50a126bb
Sina Sadeghi <sina.sa@gmail.com>
2017-03-15 16:05:26 +1100

    clean up util.py

--- shsmc/common/util.py
+++ shsmc/common/util.py
@@ -1,24 +1,24 @@
-from base64 import b64decode
-from nacl.encoding import RawEncoder
+""" shsmc utility methods
+"""
+from binascii import Error as BinasciiError
+from nacl.encoding import Base64Encoder
 from nacl.signing import SignedMessage
 from nacl.bindings import crypto_sign_BYTES
+from flask_restful import abort
 
 def reconstruct_signed_message(signed_message):
     """ hacky method for reconstructing signed messages as
         a PyNaCl SignedMessage object.
     """
 
-    tmp_encoder = RawEncoder
     try:
-        tmp_signed_message = tmp_encoder.encode(b64decode(signed_message))
+        tmp_signed_message = Base64Encoder.decode(signed_message)
         recon_signed_message = SignedMessage._from_parts(
-            tmp_encoder.encode(
-                tmp_signed_message[crypto_sign_BYTES]),
-            tmp_encoder.encode(
-                tmp_signed_message[crypto_sign_BYTES:]),
+            tmp_signed_message[:crypto_sign_BYTES],
+            tmp_signed_message[crypto_sign_BYTES:],
             tmp_signed_message)
-    except TypeError:
-        print "Not a valid signed message."
+    except (TypeError, BinasciiError):
+        abort(400,
+              message="The provided signed_message is not valid.")
 
     return recon_signed_message
-
